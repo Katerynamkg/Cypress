f11ea78abca7593310eb6c7140fd069ac2c0d638
Kateryna <ekaterinamkg@gmail.com>
2026-03-09 17:52:50 +0100

    fix eslint config

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,17 @@
 const { defineConfig } = require('cypress');
 
-module.exports = defineConfig({
-  allowCypressEnv: false,
+// module.exports = defineConfig({
+//   allowCypressEnv: false,
 
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//   },
+// });
+
+module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'https://guest:welcome2qauto@qauto.forstudy.space',
   },
 });
