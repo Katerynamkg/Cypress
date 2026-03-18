bdebaa5e3f06f55a74944a271e9375fa42bd4893
Kateryna <ekaterinamkg@gmail.com>
2026-03-18 20:49:16 +0100

    Add five API requests

--- cypress/e2e/five-requests.cy.js
+++ cypress/e2e/five-requests.cy.js
@@ -1,13 +1,14 @@
 /// <reference types="cypress" />
 import users from '../fixtures/users.json';
 
-describe('Publick API requests', () => {
+describe('Public API requests', () => {
   let sid;
   let carID;
   const today = new Date().toISOString().split('T')[0];
+  let expensesID;
 
   before(() => {
-    cy.request('POST', 'api/auth/signin', {
+    cy.request('POST', '/api/auth/signin', {
       email: users.correctUser.email,
       password: users.correctUser.password,
     }).then((response) => {
@@ -15,19 +16,20 @@ describe('Publick API requests', () => {
       sid = headers['set-cookie'][0].split(';')[0];
     });
   });
+
   it('Delete all cars', () => {
     cy.request({
       url: 'api/cars',
       method: 'GET',
       headers: { Cookie: sid },
     }).then((response) => {
-      (response.body.data.forEach((car) => {
+      response.body.data.forEach((car) => {
         cy.request({
           method: 'DELETE',
           url: `api/cars/${car.id}`,
           headers: { Cookie: sid },
         });
-      }));
+      });
       expect(response.status).to.eq(200);
     });
   });
@@ -57,12 +59,35 @@ describe('Publick API requests', () => {
     });
   });
 
-  // {"status":"ok","data":{"id":488468,"carBrandId":1,"carModelId":1,"initialMileage":99999,"updatedMileageAt":"2026-03-05T14:09:14.955Z","carCreatedAt":"2026-03-05T14:09:14.955Z","mileage":99999,"brand":"Audi","model":"TT","logo":"audi.png"}}
-
   it('Add expenses', () => {
     cy.request({
       url: 'api/expenses',
       method: 'POST',
+      body: {
+        carId: carID,
+        reportedAt: today,
+        mileage: 100000,
+        liters: 11,
+        totalCost: 11,
+        forceMileage: false,
+      },
+      headers: {
+        Cookie: sid,
+      },
+    }).then((response) => {
+      cy.log(JSON.stringify(response.body.data));
+      expensesID = response.body.data.id;
+      cy.log(expensesID);
+      expect(response.status).to.eq(200);
+      expect(response.body.data).to.have.property('id');
+      expect(response.body.data).to.have.property('carId');
+    });
+  });
+
+  it('Edit an expense', () => {
+    cy.request({
+      url: `api/expenses/${expensesID}`,
+      method: 'PUT',
       body: {
         carId: carID,
         reportedAt: today,
@@ -82,15 +107,15 @@ describe('Publick API requests', () => {
     });
   });
 
-  //   it.only("delete car", ()=>{
-  //       cy.request({
-  //         url: "/cars/{id}",
-  //       method: "DELETE",
-  //       headers: {
-  //       Cookie: sid,
-  //       }
-  //   }).then((response) =>{
-  //         cy.log(response.status)
-  //       })
-  // })
+  it('Delete an expense', () => {
+    cy.request({
+      url: `api/expenses/${expensesID}`,
+      method: 'DELETE',
+      headers: {
+        Cookie: sid,
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+    });
+  });
 });
